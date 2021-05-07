15b8c8784dc9165b637e1a49e0c854b74f09a4c5
JB <cookat@cookatui-MacBookPro.local>
2021-05-07 18:44:11 +0900

    nuxt first commit

---
+++ layouts/showcase.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <navigation />
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>View</el-dropdown-item>
+              <el-dropdown-item>Add</el-dropdown-item>
+              <el-dropdown-item>Delete</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>Tom</span>
+        </el-header>
+
+        <el-main>
+          <Nuxt />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Navigation from '~/components/Navigation'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
---
+++ pages/showcase.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Nuxt Showcase</h1>
+    <el-table :data="tableData">
+      <el-table-column prop="name" label="Name" width="120">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'showcase',
+  data() {
+    return {
+      tableData: [
+        { name: '제이비' },
+        { name: '쿠마' },
+        { name: '가펠' }
+      ]
+    }
+  }
+}
+</script>
